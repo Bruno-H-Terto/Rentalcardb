97f654016c5141298e804489bf92fb4c08edda72
Bruno-H-Terto <herculanoterto@gmail.com>
2024-10-03 02:56:40 -0300

    Concluído atividade 5

---
+++ 4 - Criando a tabela POSITIONS.sql
@@ -0,0 +1,12 @@
+-- Criando a tabela POSITIONS
+CREATE TABLE POSITIONS (
+    ID INTEGER PRIMARY KEY,
+    DESCRIPTION VARCHAR(50) NOT NULL
+);
+
+INSERT INTO POSITIONS (DESCRIPTION)
+VALUES ('Gerente de Vendas'),
+       ('Gerente de compras'),
+       ('Vendedor'),
+       ('Mecânico'),
+       ('Assistente Administrativo');
