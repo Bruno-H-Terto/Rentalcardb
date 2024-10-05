ce9ab846ff4741ec9c3758730573d2dbe0eff5c0
Bruno-H-Terto <herculanoterto@gmail.com>
2024-10-04 22:43:54 -0300

    Conculído atividades 9 e 10

---
+++ 8 - Query para update de e-mail de client.sql
@@ -0,0 +1,5 @@
+-- Query para update de e-mail da cliente Carolina
+UPDATE CUSTOMERS 
+SET EMAIL = 'carolina@campuscode.com.br'
+WHERE ID = 9;
+-- WHERE NAME = 'Carolina';
---
+++ 9 - Update da data de nascimento.sql
@@ -0,0 +1,5 @@
+-- Query para update da data de nascimento da cliente Josefa
+UPDATE CUSTOMERS
+SET BIRTH_DATE = '1986-06-19'
+WHERE ID = 4;
+-- WHERE NAME = 'Josefa';
